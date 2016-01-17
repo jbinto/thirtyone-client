f1f4f122fa4fb15a7b22b5db6f7d766b245f6975
Jesse Buchanan <hello@jessebuchanan.ca>
2016-01-16 22:41:18 -0500

    Remove mocha CLI scripts and helper

--- test/test_helper.js
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-import sinon from 'sinon';
-import chai from 'chai';
-import chaiImmutable from 'chai-immutable';
-
-chai.use(chaiImmutable);
-
-global.expect = expect;
-global.sinon = sinon;
