6c091c94e8bc8151730e857b637a7d94875008a7
Jesse Buchanan <hello@jessebuchanan.ca>
2016-01-16 22:39:51 -0500

    remove dirty-chai from mocha tests too

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,11 +1,9 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
-import dirtyChai from 'dirty-chai';
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
 
 chai.use(chaiImmutable);
-chai.use(dirtyChai);
 
 global.expect = expect;
 global.sinon = sinon;
