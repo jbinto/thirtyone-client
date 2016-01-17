eac6d3118a8606818de378b1d6da3a8e07b0fc1a
Jesse Buchanan <hello@jessebuchanan.ca>
2016-01-16 22:34:03 -0500

    Remove dirty-chai since it doesn't work with karma

--- test/sanity.spec.js
+++ test/sanity.spec.js
@@ -1,7 +1,10 @@
+/* globals describe, it */
+/* eslint-disable no-unused-expressions */
+
 import { expect } from 'chai';
 
 describe('hello world', () => {
   it('works!', () => {
-    expect(true).to.be.true();
+    expect(true).to.be.true;
   });
 });
